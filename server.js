8afb18882a64de9fd686aa36ce29776bc567f6d6
Aman Pal <amanpal6000@gmail.com>
2025-11-09 23:22:09 +0530

    Initial commit - backend setup complete

--- server.js
+++ server.js
@@ -29,10 +29,7 @@ app.use(
 // CORS setup
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://golden-admin-rouge.vercel.app"
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
